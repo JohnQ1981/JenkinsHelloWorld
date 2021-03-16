89a82f9ab55fe26ec9848ce9b8e0423f13da213b
JohnQ1981 <69947888+JohnQ1981@users.noreply.github.com>
2021-03-15 22:04:32 -0500

    Update HelloWorldForJenkins.java

--- HelloWorldForJenkins.java
+++ HelloWorldForJenkins.java
@@ -3,7 +3,8 @@ package helloworld;
 public class HelloWorldForJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World for Jenkins, version2");
+		System.out.println(" Jenkins, version2");
+		System.out.println("trying webhook for Jenkins, version2");
 
 	}
 
